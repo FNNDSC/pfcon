a589f6f69bae2edcd57111e26bbf2f5b435d49f7
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-09-14 17:20:59 -0400

    Update to 1.2.4 / pfurl 1.3.3

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ##############
-pfcon  v1.2.3
+pfcon  v1.2.4
 ##############
 
 .. image:: https://badge.fury.io/py/pfcon.svg
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfcon',
-      version          =   '1.2.3',
+      version          =   '1.2.4',
       description      =   '(Python) Process and File Controller',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
