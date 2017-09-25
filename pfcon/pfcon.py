d70176af937c401b233a985d6df5f2c9d6c97007
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-09-25 15:25:21 -0400

    Fix bug with pman/pfioh IP setting w/o HOST_IP or container env vars

--- pfcon/pfcon.py
+++ pfcon/pfcon.py
@@ -1316,7 +1316,9 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     def setup(self, **kwargs):
         global G_b_httpResponse
         global Gd_tree
-        str_defIP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        str_defIP       = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        str_defIPpman   = str_defIP
+        str_defIPpfioh  = str_defIP
 
         if 'HOST_IP' in os.environ:
             str_defIP       = os.environ['HOST_IP']
