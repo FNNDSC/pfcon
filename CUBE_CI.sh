80e12ff38992978d12513b66c67fb5270d3a55c5
emilio <emilio@dhcp-10-20-1-60.bss.redhat.com>
2018-02-05 14:12:38 -0500

    semantics

--- CUBE_CI.sh
+++ CUBE_CI.sh
@@ -2,9 +2,10 @@
 set -ev
 cd ..
 git clone https://github.com/FNNDSC/ChRIS_ultron_backEnd.git
-cd pfcon/
+pushd pfcon/
 docker build -t fnndsc/pfcon:latest .
-cd ../ChRIS_ultron_backEnd/
+popd
+pushd ChRIS_ultron_backEnd/
 docker-compose up -d
 docker-compose exec chris_dev_db sh -c 'while ! mysqladmin -uroot -prootp status 2> /dev/null; do sleep 5; done;'
 docker-compose exec chris_dev_db mysql -uroot -prootp -e 'GRANT ALL PRIVILEGES ON *.* TO "chris"@"%"'
