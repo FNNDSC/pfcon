9daea6702c5cc2d8bdd7d4f4d69527cce663044d
Jorge <jbernal0019@yahoo.es>
2020-10-28 16:56:11 -0400

    Create local data dir

--- pfcon/services.py
+++ pfcon/services.py
@@ -6,6 +6,9 @@ from abc import ABC, abstractmethod
 from flask import g, current_app as app
 
 
+logger = logging.getLogger(__name__)
+
+
 class Service(ABC):
     """
     Abstract base class for the coordinated services.
@@ -15,6 +18,15 @@ class Service(ABC):
 
         # base url of the service
         self.base_url = base_url
+
+        # local data dir to store zip files before transmitting to the remote
+        self.data_dir = os.path.join(os.path.expanduser("~"), 'data')
+        if not os.path.exists(self.data_dir):
+            try:
+                os.makedirs(self.data_dir)  # create data dir
+            except OSError as e:
+                msg = 'Creation of dir %s failed, detail: %s' % (self.data_dir, str(e))
+                logger.error(msg)
 
     @abstractmethod
     def get(self):
@@ -60,12 +72,6 @@ class PfiohService(Service):
     """
     Class for the pfioh service.
     """
-    def __init__(self, base_url):
-        super().__init__(base_url)
-
-        # local data dir to store zip files before transmitting to the remote
-        self.data_dir = os.path.join(os.path.expanduser("~"), 'data')
-
     def get(self):
         super().get()
         print("The enrichment from PfiohService")
